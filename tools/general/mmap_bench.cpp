5326183e611f72f1a2898f5b7a8c67fc6fe48527
leipeng <peng@topling.cn>
2023-03-24 23:02:44 +0800

    tools: update mmap_bench.cpp

--- tools/general/mmap_bench.cpp
+++ tools/general/mmap_bench.cpp
@@ -10,10 +10,9 @@
 #include <terark/util/mmap.hpp>
 #include <terark/util/profiling.hpp>
 #include <terark/util/stat.hpp>
-#include <terark/fstring.hpp>
+#include <terark/valvec.hpp>
 #include <random>
 #include <thread>
-#include <vector>
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -26,22 +25,44 @@ void usage(const char* prog) {
 R"EOS(Usage: %s Options file_name
   Options:
     -t read threads
+    -T write threads
     -r read op num per thread
     -m mmap read
     -w size of write file with random data
+    -H huge page
     -h Show this help information
     -v Show verbose info
 )EOS", prog);
 	exit(1);
 }
 
+static void fill_rand(uint64_t* base, size_t fsize) {
+    std::mt19937_64 rnd(fsize);
+    for (size_t i = 0; i < fsize/(4096/8); i++) {
+        uint64_t rv = rnd();
+        for (size_t j = 0; j < 4096/8; ++j) base[j] = rv;
+	base += 4096/8;
+    }
+}
+static void w_thr_proc(size_t i, size_t tnum, void* base, size_t fsize) {
+    size_t per_size = (fsize/4096/tnum) * 4096;
+    size_t thr_size = i+1==tnum ? fsize - per_size*(tnum-1) : per_size;
+    size_t thr_ppos = i * per_size;
+    fprintf(stderr, "w_thr_proc: %3zd ppos = %7.3f GB size = %.3f GB\n",
+            i, double(thr_ppos)/(1<<30), double(thr_size)/(1<<30));
+    fill_rand((uint64_t*)((char*)base + thr_ppos), thr_size);
+};
+
+
 int main(int argc, char* argv[]) {
     size_t fsize = 0;
     size_t thr_num = 1;
+    size_t w_thr_num = 1;
     size_t rd_num = 0;
     bool mmap_read = false;
+    bool mmap_huge = false;
 	for (;;) {
-		int opt = getopt(argc, argv, "r:t:mw:");
+		int opt = getopt(argc, argv, "r:t:T:mw:hH");
 		switch (opt) {
 		case -1:
 			goto GetoptDone;
@@ -55,12 +76,18 @@ int main(int argc, char* argv[]) {
         case 't':
             thr_num = strtoul(optarg, NULL, 10);
             break;
+        case 'T':
+            w_thr_num = strtoul(optarg, NULL, 10);
+            break;
         case 'm':
             mmap_read = true;
             break;
 		case 'v':
 		//	verbose = true;
 			break;
+		case 'H':
+		    mmap_huge = true;
+		    break;
 		case '?':
 		case 'h':
 		default:
@@ -79,21 +106,47 @@ GetoptDone:
     if (0 == thr_num) {
         thr_num = 1;
     }
+    if (0 == w_thr_num) {
+        w_thr_num = 1;
+    }
     profiling pf;
     if (fsize) {
-        int fd = open(fname, O_RDWR|O_CREAT);
+        int fd = open(fname, O_RDWR|O_CREAT, 0446);
         if (fd < 0) {
-            fprintf(stderr, "ERROR: open(%s, O_RDWR|O_CREAT) = %s\n",
+            fprintf(stderr, "ERROR: open(%s, O_RDWR|O_CREAT, 0446) = %s\n",
                     fname, strerror(errno));
             return 1;
         }
         ftruncate(fd, fsize);
-        close(fd);
-        MmapWholeFile fm(fname, true);
+    	terark::valvec<std::thread> w_thr_vec(w_thr_num-1, valvec_reserve());
+        auto run = [&](void* base) {
+            for (size_t i = 0; i < w_thr_num-1; ++i) {
+                w_thr_vec.unchecked_emplace_back(&w_thr_proc, i, w_thr_num, base, fsize);
+            }
+	    w_thr_proc(w_thr_num-1, w_thr_num, base, fsize);
+	    for (auto& t : w_thr_vec) t.join();
+        };
         auto t0 = pf.now();
-        std::mt19937_64 rnd(fsize);
-        for (size_t i = 0; i < fsize/8; i++) {
-            ((uint64_t*)fm.base)[i] = rnd();
+        if (mmap_huge) {
+          #if BOOST_OS_WINDOWS
+            #error TODO:
+          #else
+            // huge page is not supported on file backed shared mmap yet
+            auto base = mmap(NULL, fsize, PROT_READ|PROT_WRITE,
+                             MAP_SHARED, fd, 0);
+            close(fd);
+            if (MAP_FAILED == base) {
+                fprintf(stderr, "mmap(%s, len=%zd, SHARED) = %s\n",
+                        fname, fsize, strerror(errno));
+                return 1;
+            }
+			run(base);
+            munmap(base, fsize);
+          #endif
+        } else {
+            close(fd);
+            MmapWholeFile fm(fname, true);
+            run(fm.base);
         }
         auto t1 = pf.now();
         fprintf(stderr,
@@ -119,8 +172,7 @@ GetoptDone:
         }
     }
     size_t gsum = 0;
-    std::vector<std::thread> thr_vec;
-    thr_vec.reserve(thr_num-1);
+    terark::valvec<std::thread> thr_vec(thr_num-1, valvec_reserve());
     llong t0, t1;
     if (mmap_read) {
         MmapWholeFile fm(fname);
@@ -136,7 +188,7 @@ GetoptDone:
         };
         t0 = pf.now();
         for (size_t i = 0; i < thr_num-1; ++i) {
-            thr_vec.emplace_back(rd_func, i);
+            thr_vec.unchecked_emplace_back(rd_func, i);
         }
         rd_func(thr_num-1);
         for (auto& t : thr_vec) t.join();
