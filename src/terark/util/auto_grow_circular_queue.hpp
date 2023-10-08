84bf156f7f6d761a93368906c1571f8aaa174dc8
leipeng <peng@topling.cn>
2023-10-08 11:38:31 +0800

    Add AutoGrowCircularQueue2d and unit test

--- src/terark/util/auto_grow_circular_queue.hpp
+++ src/terark/util/auto_grow_circular_queue.hpp
@@ -1,7 +1,6 @@
 #pragma once
-#include <terark/util/autofree.hpp>
-#include <terark/bitmanip.hpp>
-#include <terark/util/throw.hpp>
+#include <terark/util/function.hpp>
+#include <memory>
 #include <type_traits>
 #include <string.h>
 
@@ -9,17 +8,30 @@ namespace terark {
 
 template<class T>
 class AutoGrowCircularQueue {
-    static_assert(std::is_trivially_destructible<T>::value, "T must be trivially destructible");
-    AutoFree<T> m_vec;
+    //static_assert(std::is_trivially_destructible<T>::value, "T must be trivially destructible");
+    T*          m_vec;
     size_t      m_cap;
     size_t      m_head;
     size_t      m_tail;
-public:
-    explicit AutoGrowCircularQueue(size_t cap = 256) {
+    size_t check_cap(size_t cap) {
         if ((cap & (cap-1)) != 0 || 0 == cap) {
-            THROW_STD(logic_error, "invalid cap = %zd, must be power of 2", cap);
+            TERARK_DIE("invalid cap = %zd, must be power of 2", cap);
         }
-        m_vec.alloc(cap);
+        return cap;
+    }
+public:
+    ~AutoGrowCircularQueue() {
+        if (m_head < m_tail) {
+            STDEXT_destroy_range(m_vec + m_head, m_vec + m_tail);
+        }
+        else if (m_tail < m_head) {
+            STDEXT_destroy_range(m_vec + m_head, m_vec + m_cap);
+            STDEXT_destroy_range(m_vec, m_vec + m_tail);
+        }
+    }
+    explicit AutoGrowCircularQueue(size_t cap = 256) {
+        m_vec = (T*)malloc(sizeof(T) * check_cap(cap));
+        TERARK_VERIFY(nullptr != m_vec);
         m_cap = cap;
         m_head = 0;
         m_tail = 0;
@@ -29,7 +41,7 @@ public:
         m_cap  = y.m_cap ;
         m_head = y.m_head;
         m_tail = y.m_tail;
-        memcpy(m_vec.p, y.m_vec.p, sizeof(T) * m_cap);
+        memcpy(m_vec, y.m_vec, sizeof(T) * m_cap);
     }
     AutoGrowCircularQueue& operator=(const AutoGrowCircularQueue& y) {
         if (&y != this) {
@@ -39,32 +51,38 @@ public:
         return *this;
     }
     void swap(AutoGrowCircularQueue& y) {
-        std::swap(m_vec.p, y.m_vec.p);
+        std::swap(m_vec, y.m_vec);
         std::swap(m_cap  , y.m_cap  );
         std::swap(m_head , y.m_head );
         std::swap(m_tail , y.m_tail );
     }
     const T& front() const {
         assert(((m_tail - m_head) & (m_cap - 1)) > 0);
-        return m_vec.p[m_head];
+        return m_vec[m_head];
     }
     T& front() {
         assert(((m_tail - m_head) & (m_cap - 1)) > 0);
-        return m_vec.p[m_head];
+        return m_vec[m_head];
     }
     void pop_front() {
         assert(((m_tail - m_head) & (m_cap - 1)) > 0);
+        if (!std::is_trivially_destructible<T>::value) {
+            m_vec[m_head].~T();
+        }
         m_head = (m_head + 1) & (m_cap - 1);
     }
     T pop_front_val() {
         assert(((m_tail - m_head) & (m_cap - 1)) > 0);
-        T x = m_vec.p[m_head];
+        T x(std::move(m_vec[m_head]));
+        if (!std::is_trivially_destructible<T>::value) {
+            m_vec[m_head].~T();
+        }
         m_head = (m_head + 1) & (m_cap - 1);
         return x;
     }
     // caller ensure @param output has enough space
     void pop_all(T* output) {
-        T*     base = m_vec.p;
+        T*     base = m_vec;
         size_t head = m_head;
         size_t tail = m_tail;
         if (head < tail) {
@@ -81,7 +99,7 @@ public:
     // caller ensure @param output has enough space
     void pop_n(T* output, size_t n) {
         n = std::min(n, this->size());
-        T*     base = m_vec.p;
+        T*     base = m_vec;
         size_t head = m_head;
         size_t tail = m_tail;
         if (head < tail) {
@@ -103,12 +121,33 @@ public:
         size_t tail = m_tail;
         size_t cap1 = m_cap - 1;
         if (terark_likely(((head - tail) & cap1) != 1)) {
-            m_vec.p[tail] = x;
+            new(&m_vec[tail]) T (x);
             m_tail = (tail + 1) & cap1;
         }
         else { // queue is full
             push_back_slow_path(x);
         }
+    }
+    template<class... Args>
+    void emplace_back(Args&&... args) {
+        size_t head = m_head;
+        size_t tail = m_tail;
+        size_t cap1 = m_cap - 1;
+        if (terark_likely(((head - tail) & cap1) != 1)) {
+            new(&m_vec[tail]) T (std::forward<Args>(args)...);
+            m_tail = (tail + 1) & cap1;
+        }
+        else { // queue is full
+            emplace_back_slow_path(std::forward<Args>(args)...);
+        }
+    }
+    const T& back() const {
+        assert(((m_tail - m_head) & (m_cap - 1)) > 0);
+        return m_vec[(m_tail - 1) & (m_cap - 1)];
+    }
+    T& back() {
+        assert(((m_tail - m_head) & (m_cap - 1)) > 0);
+        return m_vec[(m_tail - 1) & (m_cap - 1)];
     }
     size_t capacity() const { return m_cap; }
     size_t size() const { return (m_tail - m_head) & (m_cap - 1); }
@@ -117,13 +156,21 @@ private:
     terark_no_inline
     void push_back_slow_path(const T& x) {
         double_cap();
-        m_vec.p[m_tail] = x;
+        new(&m_vec[m_tail]) T (x);
+        m_tail = (m_tail + 1) & (m_cap - 1);
+    }
+    template<class... Args>
+    terark_no_inline
+    void emplace_back_slow_path(Args&&... args) {
+        double_cap();
+        new(&m_vec[m_tail]) T (std::forward<Args>(args)...);
         m_tail = (m_tail + 1) & (m_cap - 1);
     }
     void double_cap() {
         size_t cap = m_cap;
-        m_vec.resize(cap * 2);
-        T*     base = m_vec.p;
+        m_vec = (T*)realloc(m_vec, sizeof(T) * cap * 2);
+        TERARK_VERIFY(nullptr != m_vec);
+        T*     base = m_vec;
         size_t head = m_head;
         if (head > cap / 2) {
             memcpy(base + cap + head, base + head, sizeof(T)*(cap - head));
@@ -136,6 +183,66 @@ private:
         }
         m_cap = cap * 2;
     }
+};
+
+template<class T>
+class AutoGrowCircularQueue2d {
+    size_t m_cap_col;
+    size_t m_size;
+    AutoGrowCircularQueue<AutoGrowCircularQueue<T> > m_queue;
+    size_t check_cap(size_t cap) {
+        if ((cap & (cap-1)) != 0 || 0 == cap) {
+            TERARK_DIE("invalid cap = %zd, must be power of 2", cap);
+        }
+        return cap;
+    }
+public:
+    AutoGrowCircularQueue2d(const AutoGrowCircularQueue2d&) = delete;
+    AutoGrowCircularQueue2d& operator=(const AutoGrowCircularQueue2d&) = delete;
+    explicit AutoGrowCircularQueue2d(size_t cap_row = 256, size_t cap_col = 256)
+      : m_cap_col(this->check_cap(cap_col)), m_size(0), m_queue(cap_row) {}
+    void swap(AutoGrowCircularQueue2d& y) { this->swap(y); }
+    const T& front() const {
+        assert(m_size > 0);
+        return m_queue.front().front();
+    }
+    T& front() {
+        assert(m_size > 0);
+        return m_queue.front().front();
+    }
+    const T& back() const {
+        assert(m_size > 0);
+        return m_queue.back().back();
+    }
+    T& back() {
+        assert(m_size > 0);
+        return m_queue.back().back();
+    }
+    void pop_front() {
+        assert(m_size > 0);
+        m_queue.front().pop_front();
+        if (m_queue.empty())
+            m_queue.pop_front();
+        m_size--;
+    }
+    void push_back(const T& x) {
+        if (terark_unlikely(m_queue.empty())) {
+            m_queue.emplace_back(m_cap_col);
+        }
+        m_queue.back().push_back(x);
+        m_size++;
+    }
+    template<class... Args>
+    void emplace_back(Args&&... args) {
+        if (m_queue.empty()) {
+            m_queue.emplace_back(m_cap_col);
+        }
+        m_queue.back().emplace_back(std::forward<Args>(args)...);
+        m_size++;
+    }
+    size_t capacity() const { return (m_cap_col - 1) * (m_queue.capacity() - 1); }
+    size_t size() const { return m_size; }
+    bool  empty() const { return m_size == 0; }
 };
 
 }
--- tests/core/test_auto_grow_circular_queue.cpp
+++ tests/core/test_auto_grow_circular_queue.cpp
@@ -5,28 +5,55 @@
 #include <stdlib.h>
 #include <deque>
 
+template<int> struct Elem { // for check memory leak
+    static size_t g_cnt;
+    char val;
+    Elem(char c) : val(c) { g_cnt++; }
+    Elem(const Elem& y) : val(y.val) { g_cnt++; }
+    Elem(Elem&& y) : val(y.val) { g_cnt++; }
+    Elem& operator=(const Elem& y) = default;
+    Elem& operator=(Elem&& y) = default;
+    ~Elem() { g_cnt--; }
+    operator char() const { return val; }
+};
+template<int tag> size_t Elem<tag>::g_cnt;
+
 int main(int argc, char** argv)
 {
-	terark::AutoGrowCircularQueue<char> aq(2);
-    std::deque<char> dq;
+	terark::AutoGrowCircularQueue<Elem<1> > q1(2);
+	terark::AutoGrowCircularQueue2d<Elem<2> > q2(2, 4);
+    std::deque<char> qd;
     for (size_t i = 0; i < 1000000; i++) {
-        aq.push_back(char(i % 128));
-        dq.push_back(char(i % 128));
-        TERARK_VERIFY_EQ(aq.size(), dq.size());
+        q1.push_back(char(i % 128));
+        q2.push_back(char(i % 128));
+        qd.push_back(char(i % 128));
+        TERARK_VERIFY_EQ(q1.size(), qd.size());
+        TERARK_VERIFY_EQ(q2.size(), qd.size());
+        TERARK_VERIFY_EQ(q1.size(), Elem<1>::g_cnt);
+        TERARK_VERIFY_EQ(q2.size(), Elem<2>::g_cnt);
         if (i % 3 == 0) {
-            TERARK_VERIFY_EQ(aq.front(), dq.front());
-            aq.pop_front();
-            dq.pop_front();
+            TERARK_VERIFY_EQ(q1.front(), qd.front());
+            TERARK_VERIFY_EQ(q2.front(), qd.front());
+            q1.pop_front();
+            q2.pop_front();
+            qd.pop_front();
         }
+        TERARK_VERIFY_EQ(q1.size(), Elem<1>::g_cnt);
+        TERARK_VERIFY_EQ(q2.size(), Elem<2>::g_cnt);
     }
-    TERARK_VERIFY_EQ(aq.size(), dq.size());
-    while (!aq.empty()) {
-        TERARK_VERIFY_EQ(aq.size(), dq.size());
-        TERARK_VERIFY_EQ(aq.front(), dq.front());
-        aq.pop_front();
-        dq.pop_front();
+    TERARK_VERIFY_EQ(q1.size(), qd.size());
+    while (!q1.empty()) {
+        TERARK_VERIFY_EQ(q1.size(), qd.size());
+        TERARK_VERIFY_EQ(q2.size(), qd.size());
+        TERARK_VERIFY_EQ(q1.front(), qd.front());
+        TERARK_VERIFY_EQ(q2.front(), qd.front());
+        q1.pop_front();
+        q2.pop_front();
+        qd.pop_front();
+        TERARK_VERIFY_EQ(q1.size(), Elem<1>::g_cnt);
+        TERARK_VERIFY_EQ(q2.size(), Elem<2>::g_cnt);
     }
-    printf("AutoGrowCircularQueue test passed!\n");
+    printf("AutoGrowCircularQueue & AutoGrowCircularQueue2d test passed!\n");
 	return 0;
 }
 
